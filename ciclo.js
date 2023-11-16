5d70b0f8435edfbdd04426148950b2128da4653b
giuseppe puglia <148117552+giuseppe-puglia@users.noreply.github.com>
2023-11-16 20:35:02 +0100

    Add files via upload

---
+++ ciclo.js
@@ -0,0 +1,9 @@
+let listanomi=["franco","gennaro","valerio","giulia","eugenio"]
+
+
+for(let i = 0;i<listanomi.length; i=i+1){
+for (let j = 0;j<listanomi[i].length;j++){
+console.log(listanomi[i][j])
+
+}
+}
